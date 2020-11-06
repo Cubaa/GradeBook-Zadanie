3d2e113b1d7118f2d859581e1e050db8b81a6120
Cubaa <jakub29-11@wp.pl>
2020-11-06 18:27:34 +0100

    Rozwiązanie

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    //This should set Type to GradeBookType.Standard
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+        
+        public StandardGradeBook(string name, bool IsWeighted):base(name, IsWeighted)
+        {
+            Type= GradeBookType.Standard;
+        }
+    }
+
+
+}
